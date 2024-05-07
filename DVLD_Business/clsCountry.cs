e754b703269c4af62dc7a70f238209c5e06785b6
BinaryCodeCrafter <hamadhusindev@gmail.com>
2024-05-07 15:21:37 +0300

    added and set up the the clsCountry class with its functions

---
+++ DVLD_Business/clsCountry.cs
@@ -0,0 +1,66 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business
+{
+    public class clsCountry
+    {
+
+        public string countryName {  get; set; }
+
+        public int countryID {  get; set; }
+
+        public clsCountry()
+        {
+            this.countryID = -1;
+            this.countryName = "";
+        }
+
+        private clsCountry(int id , string name)
+        {
+            this.countryID = id;
+            this.countryName = name;
+        }
+
+        public clsCountry find(int id)
+        {
+            string name = "";
+
+            if (clsCountryData.findCountryByID(id ,ref name))
+            {
+                return new clsCountry(id , name);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public clsCountry find(string name)
+        {
+            int id = -1;
+
+            if(clsCountryData.findCountryByName(name , ref id))
+            {
+                return new clsCountry(id , name);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static DataTable getAllCounties()
+        {
+            return clsCountryData.getAllCountries();
+        }
+
+
+    }
+}
