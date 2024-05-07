31dede49e5fda4eede391e0251fe6d9fdcefc7d1
BinaryCodeCrafter <hamadhusindev@gmail.com>
2024-05-07 15:02:01 +0300

    added the clsCountryData class with getAllCountries() function

---
+++ DVLD_DataAccess/clsCountryData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsCountryData
+    {
+
+        public static DataTable getAllCountries()
+        {
+            DataTable data = new DataTable();
+
+            SqlConnection connection = new SqlConnection(SettingsDataAccess.connectionString);
+
+            string query = @"select * from Countries order by CountryName";
+
+            SqlCommand command = new SqlCommand(query , connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    data.Load(reader);
+                }
+
+                reader.Close();
+            }
+            catch(Exception e)
+            {
+                // log it
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return data;
+        }
+    }
+}
