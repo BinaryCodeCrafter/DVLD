524ac15f3dec3fa5a0a644a709c2c1e658130c24
BinaryCodeCrafter <hamadhusindev@gmail.com>
2024-05-11 14:36:27 +0300

    implemented the ctrlPersonCartWithFilter user control

---
+++ DVLD/People/Controls/ctrlPersonCardWithFilter.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DVLD.People.Controls
+{
+    partial class ctrlPersonCardWithFilter
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ctrlPersonCardWithFilter));
+            this.ctrlPersonCard1 = new DVLD.People.ctrlPersonCard();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ctrlPersonCard1
+            // 
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(3, 111);
+            this.ctrlPersonCard1.Name = "ctrlPersonCard1";
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(782, 341);
+            this.ctrlPersonCard1.TabIndex = 0;
+            this.ctrlPersonCard1.Load += new System.EventHandler(this.ctrlPersonCard1_Load);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(3, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(767, 95);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filter By :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Person ID",
+            "National No"});
+            this.comboBox1.Location = new System.Drawing.Point(120, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(192, 33);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(327, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(651, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 41);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(582, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 41);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ctrlPersonCardWithFilter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ctrlPersonCard1);
+            this.Name = "ctrlPersonCardWithFilter";
+            this.Size = new System.Drawing.Size(783, 455);
+            this.Load += new System.EventHandler(this.ctrlPersonCardWithFilter_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlPersonCard ctrlPersonCard1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DVLD/People/Controls/ctrlPersonCardWithFilter.cs
@@ -0,0 +1,123 @@
+﻿using DVLD_Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.People.Controls
+{
+    public partial class ctrlPersonCardWithFilter : UserControl
+    {
+
+        public event EventHandler<int> PersonSelected;
+
+
+        protected virtual void onPersonSelected(object sender ,int id)
+        {
+            PersonSelected?.Invoke(this, id);
+        }
+
+        private int _personID = -1;
+
+        public int PersonID
+        {
+            get { return ctrlPersonCard1.personID; }
+        }
+
+        public clsPerson person
+        {
+            get { return ctrlPersonCard1.person; }
+        }
+
+        private bool _filterEnabed = true;
+
+        public bool filterEnabled
+        {
+            get { return _filterEnabed; }
+            set {
+
+                _filterEnabed = value;
+                groupBox1.Enabled = value;
+            }
+        }
+
+        public ctrlPersonCardWithFilter()
+        {
+            InitializeComponent();
+        }
+
+
+        public void loadPersonInfo(int id)
+        {
+            comboBox1.SelectedIndex = 1;
+            textBox1.Text = id.ToString();
+            findNow();
+        }
+
+        private void findNow()
+        {
+
+            switch (comboBox1.Text) {
+
+                case "Person ID":
+                    ctrlPersonCard1.loadPerosnData(int.Parse(textBox1.Text));
+                    break;
+
+
+                case "Nationa No":
+                    ctrlPersonCard1.loadPerosnData(textBox1.Text);
+                    break;
+
+            }
+
+            if(PersonSelected != null && filterEnabled)
+            {
+                onPersonSelected(this, ctrlPersonCard1.personID);
+
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            findNow();
+        }
+
+        private void ctrlPersonCard1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ctrlPersonCardWithFilter_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 1;
+            textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmAddUpdatePerson form = new frmAddUpdatePerson();
+
+            form.DataBackEventHandler += dataBack;
+
+            form.ShowDialog();
+        }
+
+        private void dataBack(object sender, int id)
+        {
+            comboBox1.SelectedIndex = 1;
+            textBox1.Text = _personID.ToString();
+            ctrlPersonCard1.loadPerosnData(id);
+        }
+    }
+}
