fee980aa680ea8dc63650e23087de43ba80450bb
hamda <hamadhusindev@gmail.com>
2024-05-17 21:02:58 +0300

    added the clsApplicationtypData and clsApplicationType classes

---
+++ DVLD_Business/clsApplicationType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DVLD_Business
+{
+    public class clsApplicationType
+    {
+
+        public int applicationTypeID { get; set; }
+        public string applicationTypeTitle { get; set; }
+        public int applicationFees {  get; set; }
+
+        private clsApplicationType(int id , string title , int fee)
+        {
+            applicationTypeID = id;
+            applicationTypeTitle = title;
+            applicationFees = fee;
+        }
+
+
+        public static DataTable getAllApplicationTypes()
+        {
+            return clsApplicationType.getAllApplicationTypes();
+        }
+
+        public static bool uptateFees(int id , string title,  int fees)
+        {
+            return clsApplicationType.uptateFees(id, title, fees);
+        }
+
+
+
+
+    }
+}
+
+
+
+
+
+
---
+++ DVLD_DataAccess/clsApplicationTypeData.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsApplicationTypeData
+    {
+
+        public static DataTable getAllApplicationTypes()
+        {
+            DataTable data = new DataTable();
+
+            SqlConnection connection = new SqlConnection(SettingsDataAccess.connectionString);
+
+            string query = "select * from ApplicationTypes";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    data.Load(reader);
+                }
+
+                reader.Close();
+
+            }catch(Exception ex)
+            {
+                // log it
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return data;
+        }
+
+
+
+
+        public static bool updateApplication(int id , string title , int fee)
+        {
+
+            int rowsAffected = 0;
+
+            SqlConnection connection = new SqlConnection(SettingsDataAccess.connectionString);
+
+            string query = @"update ApplicationTypes set 
+                                ApplicationTitle = @title ,
+                                ApplicationFees = @fee 
+                                 where ApplicationTypeID = @id";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@fee" , fee);
+            command.Parameters.AddWithValue("@title" , title);
+            command.Parameters.AddWithValue("@id" , id);
+
+
+            try
+            {
+                connection.Open();
+
+                rowsAffected = command.ExecuteNonQuery();
+            }catch(Exception e)
+            {
+                rowsAffected = 0;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return rowsAffected > 0;
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
