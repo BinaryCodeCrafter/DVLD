7798ded0ad8e2271313c90e73929067e4bc9dd9d
hamda <hamadhusindev@gmail.com>
2024-05-21 14:51:42 +0300

    added new functions in clsApplicatinosData class

--- DVLD_DataAccess/clsApplicationsData.cs
+++ DVLD_DataAccess/clsApplicationsData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -52,6 +53,7 @@ namespace DVLD_DataAccess
         public static bool findApplicationByID(int id , ref int applicationPersonID , 
                                            ref DateTime applicationDate ,
                                            ref int applicationTypeID,
+                                           ref int applicationStatus,
                                            ref DateTime lastStatusDate,
                                            ref int paidFees, ref int createdByUserID)
         {
@@ -76,6 +78,7 @@ namespace DVLD_DataAccess
                     isFound = true;
 
                     applicationPersonID = (int)reader["ApplicationPersonID"];
+                    applicationStatus = (int)reader["ApplicationStatus"];
                     applicationDate = (DateTime)reader["ApplicationDate"];
                     applicationTypeID = (int)reader["ApplicationTypeID"];
                     lastStatusDate = (DateTime)reader["LastStatusDate"];
@@ -129,7 +132,61 @@ namespace DVLD_DataAccess
 
             return isFound;
         }
+
+
+        public static int addNewApplication(int applicationPersonID , DateTime ApplicationDate,
+                                             int applicationTypeID, int applicationStatus , DateTime lastStatusDate,
+                                             int paidFees , int createdByUserID)
+        {
+            int id = -1;
+
+            SqlConnection connection = new SqlConnection(SettingsDataAccess.connectionString);
+
+            string query = @"insert into Applicatinos (ApplicationPersonID , ApplicationDate,
+                            ApplicationTypeID , ApplicationStatus , LastStatusDate,
+                            PaidFees, CreatedByUserID) valuse 
+                            (@applicationPersonID , @ApplicationDate , @ApplicationTypeID,
+                             @applicationStatus , @lastStatusDate , @paidFees ,
+                             @createdByUserID);
+                             select scope_identity();";
+
+            SqlCommand command =new SqlCommand(query , connection);
+
+            command.Parameters.AddWithValue("@applicationPersonID" , applicationPersonID);
+            command.Parameters.AddWithValue("@applicationDate" , ApplicationDate);
+            command.Parameters.AddWithValue("@applicationTypeID" ,applicationTypeID);
+            command.Parameters.AddWithValue("@applicationStatus" , applicationStatus);
+            command.Parameters.AddWithValue("@lastStatusDate" , applicationStatus);
+            command.Parameters.AddWithValue("@paidFees" , createdByUserID);
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+                if(result != null && int.TryParse(result.ToString() , out int insertedID))
+                {
+                    id = insertedID;
+                }
+
+            }catch(Exception e)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return id;
+
+        }
+
+
+
     }
+
 }
 
 
